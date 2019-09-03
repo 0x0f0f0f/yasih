2d5ebfb7bb0fcede8db6aaf253517a9a26b81344
a <a@a.a>
2019-09-03 20:20:12 +0200

    src/Evaluator.hs

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -3,10 +3,12 @@ module Evaluator (
     module Evaluator.Show,
     module Evaluator.Primitives,
     module Evaluator.ListPrimitives,
-    module Evaluator.Errors
+    module Evaluator.Errors,
+    module Evaluator.Variables
 ) where
 
 import Evaluator.Show
 import Evaluator.Primitives
 import Evaluator.ListPrimitives
 import Evaluator.Errors
+import Evaluator.Variables
